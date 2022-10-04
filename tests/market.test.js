27a454818264c77132d065833e5cdbaa10bde826
trasua <andrew@trasua.dev>
2022-10-04 22:09:18 +0700

    token, nft auction marketplace

--- tests/market.test.js
+++ tests/market.test.js
@@ -4,21 +4,28 @@ const { ethers } = require('hardhat')
 describe('Market', () => {
     before('set up', async () => {
         this.marketAbi =
-            require('../artifacts/contracts/Market.sol/Market.json').abi
+            require('../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json').abi
         this.nftAbi = require('../artifacts/contracts/NFT.sol/NFT.json').abi
+        this.tokenAbi =
+            require('../artifacts/contracts/Token.sol/Token.json').abi
 
         const ret = await Promise.all([
             ethers.getSigners(),
-            ethers.getContractFactory('Market'),
+            ethers.getContractFactory('NFTMarketplace'),
             ethers.getContractFactory('NFT'),
+            ethers.getContractFactory('Token'),
         ])
         this.signers = ret[0]
         this.Market = ret[1]
         this.NFT = ret[2]
+        this.Token = ret[3]
     })
 
     it('deploy', async () => {
-        const marketDeploy = await this.Market.deploy()
+        const marketDeploy = await this.Market.deploy(
+            this.signers[0].address,
+            50
+        )
         await marketDeploy.deployed()
         this.Market = new ethers.Contract(
             marketDeploy.address,
@@ -33,6 +40,19 @@ describe('Market', () => {
             this.nftAbi,
             this.signers[0]
         )
+
+        const tokenDeploy = await this.Token.deploy()
+        await tokenDeploy.deployed()
+        this.Token = new ethers.Contract(
+            tokenDeploy.address,
+            this.tokenAbi,
+            this.signers[0]
+        )
+    })
+
+    it('mint token', async () => {
+        const balance = await this.Token.balanceOf(this.signers[0].address)
+        expect(balance.toString()).to.equal('5000000000000000000000000')
     })
 
     it('mint nft', async () => {
@@ -63,18 +83,32 @@ describe('Market', () => {
         }
     })
 
-    it('market listing nft', async () => {
-        const tokenId = 0
-        const listed = await this.Market.listItem(
-            this.NFT.address,
-            tokenId,
-            ethers.utils.parseEther('100')
-        )
-        await listed.wait()
+    it('add pay token', async () => {
+        const added = await this.Market.addPayableToken(this.Token.address)
+        await added.wait()
+        const payableTokens = await this.Market.getPayableTokens()
+        expect(payableTokens[0]).to.equal(this.Token.address)
     })
 
-    it('get listing', async () => {
-        const listing = await this.Market.getListing(this.Market.address, 0)
-        console.log(listing)
+    it('market listing nft', async () => {
+        const listed = await this.Market.listNft(
+            this.NFT.address,
+            0,
+            this.Token.address,
+            ethers.utils.parseEther('11111')
+        )
+        await listed.wait()
+
+        const listedNft = await this.Market.getListedNFT(this.NFT.address, 0)
+        expect(listedNft.nft).to.equal(this.NFT.address)
+        expect(listedNft.tokenId).to.equal(0)
+        expect(listedNft.seller).to.equal(this.signers[0].address)
+        expect(listedNft.payToken).to.equal(this.Token.address)
+        expect(listedNft.price.toString()).to.equal(
+            ethers.utils.parseEther('11111')
+        )
+        expect(listedNft.sold).to.equal(false)
     })
+
+    it('', async () => {})
 })
